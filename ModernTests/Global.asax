e103be533a198bb8dc0054637c9db5343ec9512e
oblomingo <oblomingo@gmail.com>
2014-10-27 14:20:40 +0200

    Initial commit, material design added

---
+++ ModernTests/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ModernTests.MvcApplication" Language="C#" %>
---
+++ ModernTests/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ModernTests.Startup))]
+namespace ModernTests
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
